016cdcb792aea5a3aeea1502eb5b06cfbe01f2f2
Grellard Adrien <grelard.adrien@gmail.com>
2020-11-29 02:33:02 +0100

    upd

--- src/adri_udp.cpp
+++ src/adri_udp.cpp
@@ -4,6 +4,9 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <adri_tools.h>
+#include <adri_wifiConnect.h>
+
+// #define DEBUG_UDP
 
 void adri_udp_parse(String s){
 	// Serial.printf("\n[adri_udp_parse]\n%s\n", s.c_str());
@@ -11,6 +14,8 @@ void adri_udp_parse(String s){
 
 adri_udp::adri_udp(){
 	_parse = adri_udp_parse;
+	// _timerSendPacket 	= new adri_timer(25, "", false);
+	// _timerRecPacket 	= new adri_timer(10, "", false);
 }
 
 void adri_udp::set_port(uint16_t port) {
@@ -32,14 +37,77 @@ IPAddress adri_udp::remoteIP(){
 	return _server.remoteIP();
 }
 
-bool adri_udp::receive(String * receive_buffer) {
-   int packet_size = _server.parsePacket();
-   if (!packet_size) return false;
-   char buf[512];
-   int length = _server.read(buf, 512);
-   if (length>0) buf[length]=0;
-   * receive_buffer = String(buf);
+// #define UDP_IN_MAXSIZE 8192 1472
+#define UDP_IN_MAXSIZE 1024
+// #define UDP_OUT_MAXSIZE 1024
+
+char adri_udp_packetBuffer[UDP_IN_MAXSIZE + 1]; 
+// char adri_udp_outPacketBuffer[UDP_OUT_MAXSIZE + 1]; 
+
+bool adri_udp::receive() {
+	// if (!_timerRecPacket->isActivate()) {
+	// 	_timerRecPacket->activate();
+	// } else {
+	// 	if (!_timerRecPacket->loop()) return false;
+	// 	else {
+	// 		_timerRecPacket->activate(false);
+	// 	}
+	// }
+
+	uint16_t packet_size = _server.parsePacket();
+
+	if (!packet_size) return false;
+
+
+	if (packet_size > UDP_IN_MAXSIZE || packet_size < 1) return false;
+
+	int length = _server.read(adri_udp_packetBuffer, UDP_IN_MAXSIZE);
+
+	adri_udp_packetBuffer[length]=0;
+
+	#ifdef DEBUG_UDP
+		fsprintf("\n[adri_udp::receive][size: %d]\n%s\n", packet_size, adri_udp_packetBuffer);
+	#endif
+
    return true;
+}
+
+void adri_udp::send(String transmit_buffer) {
+
+	// if (!_timerSendPacket->isActivate()) {
+	// 	_timerSendPacket->activate();
+	// } else {
+	// 	if (!_timerSendPacket->loop()) return;
+	// 	else {
+	// 		_timerSendPacket->activate(false);
+	// 	}
+	// }
+
+	unsigned int msg_length = transmit_buffer.length();
+
+	if (msg_length ==0) return;
+
+	byte* p = (byte*)malloc(msg_length);
+	memcpy(p, transmit_buffer.c_str(), msg_length);
+	// sprintf(adri_udp_outPacketBuffer,"%s",transmit_buffer.c_str());
+
+	_server.beginPacket(_server.remoteIP(), _server.remotePort());
+
+	_server.write(p, msg_length);		
+
+	_server.endPacket();	
+
+#ifdef DEBUG_UDP
+	// char buffer[100];
+	// sprintf(buffer, "\n[size] %d\n", size);
+	// debugPrint(String(buffer));
+
+	// Serial.printf("\n[adri_udp::send][size: %d]\n%s\n",  size, transmit_buffer.c_str());
+	// fsprintf("\n[adri_udp::send][size: %d]\n%s\n",  msg_length, transmit_buffer.c_str());
+
+#endif  
+
+	free(p);
 }
 
 void adri_udp::send_toIp(String transmit_buffer, const char * ip, uint16_t port) {
@@ -50,7 +118,7 @@ void adri_udp::send_toIp(String transmit_buffer, const char * ip, uint16_t port)
 	sprintf(buf,"%s",transmit_buffer.c_str());
 	_server.beginPacket(ip, port);
 	_server.write(buf);			
-	_server.endPacket();	
+	_server.endPacket();
 #ifdef DEBUG_UDP
 	Serial.printf("\n[size] %d\n%s\n",  size, transmit_buffer.c_str());
 #endif  
@@ -63,28 +131,9 @@ void adri_udp::send_toIp(String transmit_buffer, String s_ip, String s_port) {
 	send_toIp(transmit_buffer, ip, port) ;
 }
 
-void adri_udp::send(String transmit_buffer) {
-    int size = transmit_buffer.length();
-    if (size==0)return;
-	char buf[size+1];	
-	sprintf(buf,"%s",transmit_buffer.c_str());
-	_server.beginPacket(_server.remoteIP(), _server.remotePort());
-	_server.write(buf);			
-	_server.endPacket();	
-
-	// delay(100);
-#ifdef DEBUG_UDP
-	// char buffer[100];
-	// sprintf(buffer, "\n[size] %d\n", size);
-	// debugPrint(String(buffer));
-	 
-	Serial.printf("\n[adri_udp::send][size: %d]\n%s\n",  size, transmit_buffer.c_str());
-#endif  
-}
-
-void adri_udp::parse(String * udp_msg) { 
-	String msg = *udp_msg;	
-	_parse(msg);
+void adri_udp::parse() { 
+	_parse(String(adri_udp_packetBuffer));
+	yield();
 
 }
 void adri_udp::parse_set_func(parse_f f) {
@@ -96,7 +145,7 @@ void adri_udp::start(){
 }
 
 void adri_udp::loop() {
-	if (receive(&_receive_buffer)) parse(&_receive_buffer);
+	if (receive()) parse();
 }
 
 
@@ -119,18 +168,27 @@ void adri_udpMulti::parse_set_func(parse_f f) {
 	_parse = f;
 }
 
+char adri_udpMulti_packetBuffer[UDP_IN_MAXSIZE + 1]; 
+
 bool adri_udpMulti::receive(String * receive_buffer) {
-   int packet_size = _server.parsePacket();
-   if (!packet_size) return false;
-   char buf[1024];
-   int length = _server.read(buf, 1024);
-   if (length>0) buf[length]=0;
-   * receive_buffer = String(buf);
-   return true;
+
+	uint16_t packet_size = _server.parsePacket();
+
+	if (!packet_size) return false;
+
+	if (packet_size > UDP_IN_MAXSIZE || packet_size < 3) return false;
+
+	int length = _server.read(adri_udpMulti_packetBuffer, UDP_IN_MAXSIZE);
+
+	adri_udpMulti_packetBuffer[length]=0;
+
+	* receive_buffer = String(adri_udpMulti_packetBuffer);
+
+
+   	return true;
 }	
 
 void adri_udpMulti::send(String transmit_buffer) {
-	if (WiFi.status() != WL_CONNECTED) return;
 
     int size = transmit_buffer.length();
 	char buf[size+1];
--- src/adri_udp.h
+++ src/adri_udp.h
@@ -2,11 +2,14 @@
       	#define ADRI_UDP_H
 		#include <WiFiUdp.h>
 		#include <arduino.h>
+		#include <adri_timer.h>
 
 		typedef void (*parse_f)(String s);  
 
 		class adri_udp
 		{
+			// adri_timer 	* _timerSendPacket;
+			adri_timer 	* _timerRecPacket;
 			uint16_t 	_port 			= 9100;
 			WiFiUDP 	_server;
 			String 		_receive_buffer;
@@ -18,11 +21,11 @@
 			void set_port(uint16_t port);
 			void remote_print();
 			IPAddress remoteIP();
-			bool receive(String * receive_buffer) ;
+			bool receive() ;
 			void send_toIp(String transmit_buffer, const char * ip, uint16_t port) ;
 			void send_toIp(String transmit_buffer, String s_ip, String s_port);
 			void send(String transmit_buffer);
-			void parse(String * udp_msg);
+			void parse();
 			void parse_set_func(parse_f f);	
 			void start();
 			void loop();		
